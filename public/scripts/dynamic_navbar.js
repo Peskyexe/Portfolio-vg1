4af880e55d9c3cb80566bc9a12fe9c705349e7b3
Peskyexe <tthdarklama08@gmail.com>
2026-01-29 10:33:33 +0100

    bilde karusell script refactor

--- public/scripts/dynamic_navbar.js
+++ public/scripts/dynamic_navbar.js
@@ -1,14 +1,16 @@
+// Henter in knapp elementene for sidebaren
 const menu_close_button = document.querySelector(".menu-close-button")
 const menu_open_button = document.querySelector(".menu-open-button")
 
+// Legger til EventListeners som lytter etter om brukeren trykker på en av knappene, å kjører den tilsvarene funksjonen for den knappen
 menu_close_button.addEventListener("click", hideSidebar)
 menu_open_button.addEventListener("click", showSidebar)
 
+// Funksjoner for å åpne og lukke sidebaren
 function showSidebar() {
 	const sidebar = document.querySelector(".nav-links-sidebar");
 	sidebar.style.display = "flex";
 }
-
 function hideSidebar() {
 	const sidebar = document.querySelector(".nav-links-sidebar");
 	sidebar.style.display = "none";
--- public/scripts/projects/image_carousel.js
+++ public/scripts/projects/image_carousel.js
@@ -1,42 +1,37 @@
+// Funksjon for å sette opp / initialisere en bilde karusell
+function initializeCarousel(container) {
+    // Henter inn alle knappene og bildene I karusellen
+    const buttons = container.querySelectorAll('.project-button');
+    const images = container.querySelectorAll('.project-image');
+
+    // Setter den første knappen og første bilde som aktiv
+    var current_index = 0;
+    buttons[current_index].classList.add('project-button-active');
+    images[current_index].classList.add('project-image-active');
+
+    // For hver knapp I karusellen, set opp EventListener
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            // Henter in knappen sin index i "buttons" arrayen
+            let clicked_index = Array.from(buttons).indexOf(event.currentTarget);
+
+            // Fjerner aktiv klassene fra de forigje aktive elementene
+            buttons[current_index].classList.remove('project-button-active');
+            images[current_index].classList.remove('project-image-active');    
+
+            // Legger til aktiv klassen til de nye elementene
+            buttons[clicked_index].classList.add('project-button-active');
+            images[clicked_index].classList.add('project-image-active');
+
+            // Oppdaterer indeksen
+            current_index = clicked_index;
+        })
+    });
+}
+
+// Henter inn containerene for alle bilde karusell elementene, å bruker initialisere funksjonen for hver karusell.
 const carousels = document.querySelectorAll(".project-container");
 carousels.forEach(carousel => {
     console.log("Initializing image carousel for: ", carousel);
     initializeCarousel(carousel);
 });
-
-function initializeCarousel(container) {
-    const buttons = container.querySelectorAll('.project-images-buttons button');
-    const images = container.querySelectorAll('.project-image');
-
-    buttons.forEach((button) => {
-        button.addEventListener('click', (event) => {
-            // Henter in knappen sin index i "buttons" arrayen
-            const clickedIndex = Array.from(buttons).indexOf(event.currentTarget);
-            
-            // Fjerner alle aktiv klassene til de forigje knappene og bildene
-            changeClasses(buttons, "project-button-active", "project-button");
-            changeClasses(images, "project-image-active", "project-image");
-
-            // Legger til aktiv klassene til den nye knappen og bilde
-            button.classList.add("project-button-active");
-            images[clickedIndex].classList.add("project-image-active");
-        })
-    });
-
-    // Setter det første bilde og den første knappen til å være aktiv
-    buttons[0].classList.remove("project-button")
-    images[0].classList.remove("project-image")
-    buttons[0].classList.add("project-button-active")
-    images[0].classList.add("project-image-active")
-}
-
-// Funksjon til å bytte mellom 2 forsjkelige klasser
-function changeClasses(array_element, class_to_remove, class_to_add) {
-    array_element.forEach(element => {
-        if (element.classList.contains(class_to_remove)) {
-            element.classList.remove(class_to_remove);
-            element.classList.add(class_to_add)
-        }
-    });    
-}
-
